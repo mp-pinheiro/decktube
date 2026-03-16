cfdcb43014c1b9641d7d20859817855443b1e795
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2026-03-16 02:10:37 -0300

    feat: version from package.json, add release workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
   plugins: [
     react(),
     tailwindcss(),
