67f57c40bf4b2172bb9c28d0312a6ded7f7f92d7
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2026-03-16 03:12:38 -0300

    fix: use executableName for correct Linux binary casing

--- afterPack.cjs
+++ afterPack.cjs
@@ -5,14 +5,14 @@ exports.default = async function (context) {
   if (context.electronPlatformName !== 'linux') return
 
   const appOutDir = context.appOutDir
-  const productName = context.packager.appInfo.productFilename
-  const execPath = join(appOutDir, productName)
-  const binPath = join(appOutDir, `${productName}.bin`)
+  const execName = context.packager.executableName
+  const execPath = join(appOutDir, execName)
+  const binPath = join(appOutDir, `${execName}.bin`)
 
   renameSync(execPath, binPath)
   writeFileSync(
     execPath,
-    `#!/bin/bash\nHERE="$(dirname "$(readlink -f "$0")")"\nexec "$HERE/${productName}.bin" --no-sandbox --disable-dev-shm-usage "$@"\n`
+    `#!/bin/bash\nHERE="$(dirname "$(readlink -f "$0")")"\nexec "$HERE/${execName}.bin" --no-sandbox --disable-dev-shm-usage "$@"\n`
   )
   chmodSync(execPath, 0o755)
 }
